b603a73968fcec0a538a2207bfa05d0c1a104a20
djigaq <103529027+djigaq@users.noreply.github.com>
2023-06-16 19:33:49 +0300

    q

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
@@ -22,13 +24,11 @@ import java.util.Set;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
-    private final RoleService roleService;
     private final SuccessUserHandler successUserHandler;
 
     @Autowired
-    public WebSecurityConfig(@Lazy UserService userService, RoleService roleService, SuccessUserHandler successUserHandler) {
+    public WebSecurityConfig(@Lazy UserService userService, SuccessUserHandler successUserHandler) {
         this.userService = userService;
-        this.roleService = roleService;
         this.successUserHandler = successUserHandler;
     }
 
@@ -38,8 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/welcome").permitAll()
-                .antMatchers("/admin/**", "/api/admin").hasRole("ADMIN")
-                .antMatchers("/user/**", "/api/users").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().successHandler(successUserHandler)
@@ -50,26 +50,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
     }
+    @Component
+    public class CreateUser implements CommandLineRunner {
+        private final UserService userService;
+        private final RoleService roleService;
 
-    public void run(String... args) throws Exception {
-        Role roleAdmin = new Role("ROLE_ADMIN");
-        Role roleUser = new Role("ROLE_USER");
-        // логин = user пароль = user
-        User user1 = new User("user", "surname", 25, "user@email.ru", "user");
-        // логин = admin пароль = admin
-        User admin1 = new User("admin", "surname", 26, "admin@email.ru", "admin");
-        Set<Role> roleTwo = new HashSet<>();
-        roleTwo.add(roleUser);
-        roleTwo.add(roleAdmin);
-        Set<Role> roleOne = new HashSet<>();
-        roleOne.add(roleUser);
-        roleService.addRole(roleUser);
-        roleService.addRole(roleAdmin);
-        user1.setRoles(roleOne);
-        admin1.setRoles(roleTwo);
-        userService.addUser(user1);
-        userService.addUser(admin1);
+        public CreateUser(UserService userService, RoleService roleService) {
+            this.userService = userService;
+            this.roleService = roleService;
+        }
+
+
+        @Override
+        public void run(String... args) throws Exception {
+            Role roleAdmin = new Role("ROLE_ADMIN");
+            Role roleUser = new Role("ROLE_USER");
+            // логин = user@email.ru пароль = user
+            User user1 = new User("user", "surname", 25, "user@email.ru", "user");
+            // логин = admin@email.ru пароль = admin
+            User admin1 = new User("admin", "surname", 26, "admin@email.ru", "admin");
+            Set<Role> roleTwo = new HashSet<>();
+            roleTwo.add(roleUser);
+            roleTwo.add(roleAdmin);
+            Set<Role> roleOne = new HashSet<>();
+            roleOne.add(roleUser);
+            roleService.addRole(roleUser);
+            roleService.addRole(roleAdmin);
+            user1.setRoles(roleOne);
+            admin1.setRoles(roleTwo);
+            userService.addUser(user1);
+            userService.addUser(admin1);
+        }
     }
+
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
