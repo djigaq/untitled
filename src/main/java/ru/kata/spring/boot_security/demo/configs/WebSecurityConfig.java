3f8c32a1e5f422c9d559dd4895a97df01fbbe305
djigaq <103529027+djigaq@users.noreply.github.com>
2023-07-10 19:26:34 +0300

    0

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -33,22 +33,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/welcome").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().successHandler(successUserHandler)
-                .loginPage("/login")
-                .loginProcessingUrl("/process_login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+    protected void configure(HttpSecurity http)  {
+        try {
+            http
+                    .csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/", "/welcome").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().successHandler(successUserHandler)
+                    .loginPage("/login")
+                    .loginProcessingUrl("/process_login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
     @Component
     public class CreateUser implements CommandLineRunner {
